3e9e973d114a24768d2ebe5954345df0d0fa0a3a
Nasaee <nasaee.dev@gmail.com>
2025-10-24 00:46:24 +0700

    implement crud event

---
+++ routes/events.go
@@ -0,0 +1,114 @@
+package routes
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/Nasaee/go-gin-rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func GetEvents(context *gin.Context) {
+	events, err := models.GetAllEvents()
+	if err != nil {
+		log.Println(err)
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not get events. Try again later."})
+	}
+	context.JSON(http.StatusOK, events)
+}
+
+func GetEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		log.Println(err)
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		log.Println(err)
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not get event."})
+	}
+
+	context.JSON(http.StatusOK, event)
+}
+
+func CreateEvents(context *gin.Context) {
+	var event models.Event
+	// ShouldBindJSON : มันมีหน้าที่ อ่าน JSON จาก request body แล้วแปลงเป็น struct ที่คุณส่งเข้าไป
+	err := context.ShouldBindJSON(&event)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	err = event.Save()
+	if err != nil {
+		log.Println(err)
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create event. Try again later."})
+		return
+	}
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created.", "event": event})
+}
+
+func UpdateEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		log.Println(err)
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+
+	_, err = models.GetEventById(eventId)
+	if err != nil {
+		log.Println(err)
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not get event."})
+	}
+
+	var updatedEvent models.Event
+
+	err = context.ShouldBindJSON(&updatedEvent)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	updatedEvent.ID = eventId
+
+	err = updatedEvent.Update()
+	if err != nil {
+		log.Println(err)
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not update event. Try again later."})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "Event updated successfully."})
+}
+
+func DeleteEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		log.Println(err)
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		log.Println(err)
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not get event."})
+	}
+
+	err = event.Delete()
+	if err != nil {
+		log.Println(err)
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not delete event."})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Event deleted successfully."})
+}
